74f57bff08cba3b6911ef7b8b84c9688fbf29afb
sriranjani <sriranjani.mnv@cgg.gov.in>
2019-06-25 15:02:33 +0530

    Initial commit

---
+++ newlibrary/src/main/java/cgg/gov/in/newlibrary/method.java
@@ -0,0 +1,12 @@
+package cgg.gov.in.newlibrary;
+
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+public class method  {
+
+    public static void message() {
+        String test="1";
+        System.out.print(test);
+    }
+}
